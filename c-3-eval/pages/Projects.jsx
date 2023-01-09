e2dbf4580f8ae4ea7d5f7f12c9dc0a507ebafb99
agrawalrajat310 <agrawalrajat03@outlook.com>
2023-01-09 14:21:48 +0530

    eval push

--- c-3-eval/pages/Projects.jsx
+++ c-3-eval/pages/Projects.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-import { Text } from '@chakra-ui/react';
+import { Text } from "@chakra-ui/react";
 
-
-
-const Projects = ({gets}) => {
+const Projects = ({ gets }) => {
   return (
     <>
+      <Head>
+        <title>PROJECTS</title>
+      </Head>
+
+      <main>
+        <div className="project_main">
+          {gets.map((el) => (
+            <div key={el.id}>
+              <Link href={`${el.html_url}`}>
+                {" "}
+                <Text>{el.name}</Text>
+                <div
+                  style={{
+                    display: "flex",
+                    marginTop: "-20px",
+                  }}
+                >
+                  <Text>{el.stargazers_count}</Text>
+                  <Text>{el.forks}</Text>
+                  <Text>{el.language}</Text>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
-  )
+  );
+};
 
 export default Projects;
